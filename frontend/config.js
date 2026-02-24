b655ba81e3fa8dd609a100ed82ba06b2fd33d1d1
Sanika <sdeshkar2005@gmail.com>
2026-02-24 22:45:23 +0530

    fixed css case issue

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-export const server = "http://localhost:5000";
+export const server = "https://e-learningweb-2-2h89.onrender.com/";
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:5000";
+export const server = "https://e-learningweb-2-2h89.onrender.com/";
 export default server;
