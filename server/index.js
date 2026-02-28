7b6332eee850ada5ebf85aecb8f22b60c0800239
Sanika-deshkar <sdeshkar2005@gmail.com>
2026-02-28 09:51:13 +0530

    Update CORS origin for the application

--- server/index.js
+++ server/index.js
@@ -26,8 +26,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://eduhub-learning-flax.vercel.app"
+    "https://e-learning-eduhub.vercel.app"
   ],
   credentials: true,
 }));
